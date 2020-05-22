b6dc22d814a44912bad4ee5cf2e6ff5fd52de168
nickdraper8 <nickdraper8@gmail.com>
2020-05-22 18:47:53 -0400

    Updates

---
+++ precache-manifest.0edf9b55a0ea5564060a7b9884718c80.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a68a4d4ae614158c38e7ea5e464a5d",
+    "url": "/ReactPractice/index.html"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/ReactPractice/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d4745bef2794613bef9b",
+    "url": "/ReactPractice/static/css/main.628ca412.chunk.css"
+  },
+  {
+    "revision": "d4add6107f8fd2eb1c82",
+    "url": "/ReactPractice/static/js/2.7ec5fd90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPractice/static/js/2.7ec5fd90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4745bef2794613bef9b",
+    "url": "/ReactPractice/static/js/main.7ebacf1a.chunk.js"
+  },
+  {
+    "revision": "8ecc9f267aa62835283c",
+    "url": "/ReactPractice/static/js/runtime-main.27ab83be.js"
+  },
+  {
+    "revision": "d4aa34b4b9a56d755be25ebf63f178f9",
+    "url": "/ReactPractice/static/media/badrobot.d4aa34b4.woff"
+  }
+]);
